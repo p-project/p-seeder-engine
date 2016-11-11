af585458c9c3b1576b8af8e372b8d1595a95491c
Coac <levictor3000@hotmail.fr>
2016-11-11 20:51:08 +0100

    Basic seed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hey');
+import WebTorrent from 'webtorrent-hybrid';
+
+const client = new WebTorrent();
+const file = 'index.js';
+client.seed(file, function (torrent) {
+  console.log('started seeding %s - %s', torrent.infoHash, torrent.files[0].name);
+});
