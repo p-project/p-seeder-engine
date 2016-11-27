10e0baa25b1b2a31c9338205296c1f07b698f3db
IGI-111 <igi.222@gmail.com>
2016-11-27 14:35:37 +0100

    fixed linting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 import WebTorrent from 'webtorrent';
 import timers from 'timers';
@@ -8,33 +8,32 @@ const web = express();
 const bt = new WebTorrent();
 
 web.get('/list', (req, res) => {
-    let torrentHashes = bt.torrents.map((t) => t.infoHash);
-    res.json(torrentHashes);
+  let torrentHashes = bt.torrents.map((t) => t.infoHash);
+  res.json(torrentHashes);
 });
 
 web.post('/add/:infoHash', (req, res) => {
-    let infoHash = req.params.infoHash
-    bt.add(infoHash, (torrent) => {
-        res.send(torrent.infoHash);
-    });
+  let infoHash = req.params.infoHash;
+  bt.add(infoHash, (torrent) => {
+    res.send(torrent.infoHash);
+  });
 });
 
 web.delete('/delete/:infoHash', (req, res) => {
-    let infoHash = req.params.infoHash;
-    bt.remove(infoHash, (err) => {
-        res.send(err);
-    });
+  let infoHash = req.params.infoHash;
+  bt.remove(infoHash, (err) => {
+    res.send(err);
+  });
 });
 
 // print the status of known torrents every now and then
 timers.setInterval(() => {
-    bt.torrents.forEach((t) => {
-        console.log(t.infoHash + '\n' +
-            '\tUp: ' + t.uploadSpeed + ' ' + t.uploaded + '\n' +
-            '\tDown: ' + t.downloadSpeed + ' ' + t.downloaded
-        );
-    });
+  bt.torrents.forEach((t) => {
+    console.log(t.infoHash + '\n' +
+      '\tUp: ' + t.uploadSpeed + ' ' + t.uploaded + '\n' +
+        '\tDown: ' + t.downloadSpeed + ' ' + t.downloaded
+    );
+  });
 }, 1000);
-
 
 web.listen(2342);
