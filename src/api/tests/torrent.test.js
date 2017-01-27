713ebfd19793522b54b110089e2f9484ed4c4fbe
Victor Le <levictor3000@hotmail.fr>
2017-01-27 18:23:14 +0100

    Init test

---
+++ src/api/tests/torrent.test.js
@@ -0,0 +1,11 @@
+import chai, { expect } from 'chai'
+
+describe('## Torrent APIs', () => {
+  describe('# POST Seed a video', () => {
+    it('ok', (done) => {
+      (async() => {
+        done()
+      })()
+    })
+  })
+})
--- wip.test.js
+++
@@ -1,9 +0,0 @@
-import chai, { expect } from 'chai';
-
-chai.config.includeStack = true;
-
-describe('No test', () => {
-  it('should pass', () => {
-    expect().to.not.exist;
-  });
-});
