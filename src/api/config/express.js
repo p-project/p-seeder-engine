fd057c2a5aaf59c7d1e0306a358a68631ed1ce2a
Victor Le <levictor3000@hotmail.fr>
2017-01-27 21:28:36 +0100

    Cors allow methods

--- src/api/config/express.js
+++ src/api/config/express.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS')
   res.header('Access-Control-Allow-Headers', 'access-control-allow-origin, content-type')
   next()
 })
