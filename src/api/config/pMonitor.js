f695e721856fd16a634090b213dc372cf63190a7
Victor Le <levictor3000@hotmail.fr>
2017-04-03 14:54:00 +0200

    Added tracker monitor scrape function

--- src/api/config/pMonitor.js
+++ src/api/config/pMonitor.js
@@ -3,6 +3,7 @@
 *
 **/
 import rp from 'request-promise'
+import TrackerClient from 'bittorrent-tracker'
 
 const urlPMonitor = process.env.P_MONITOR_API_URL
 
@@ -14,4 +15,26 @@ export async function getSeedTorrent () {
 
 export function notifySeeding () {
   return 'lol WIP'
+}
+
+export function scrape () {
+  var requiredOpts = {
+    infoHash: new Buffer('aaa67059ed6bd08362da625b3ae77f6f4a075aaa'),
+    peerId: new Buffer('01234567890123456789'),
+    announce: [process.env.ANNOUNCE_URL_UDP],
+    port: 4561
+  }
+  var client = new TrackerClient(requiredOpts)
+
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      reject('scrape timed out after 5s')
+    }, 5000)
+    client.on('warning', reject)
+    client.on('error', reject)
+
+    client.scrape()
+
+    client.on('scrape', resolve)
+  })
 }
