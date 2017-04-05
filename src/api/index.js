dfcf136b756b5338146345fd369a844c16354342
IGI-111 <igi.222@gmail.com>
2017-04-05 13:47:24 +0200

    removed unnecessary indirection in paths

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-import app from './config/express'
-
-const port = process.env.P_SEEDER_ENGINE_PORT
-app.listen(port, () => console.log('listening on port ' + port))
-
-export default app
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
-require('dotenv').config()
-require('babel-polyfill')
-require('./api')
+
+import app from './config/express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.P_SEEDER_ENGINE_PORT
+app.listen(port, () => console.log('listening on port ' + port))
+
+export default app
